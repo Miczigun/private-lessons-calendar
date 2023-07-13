21be4bcbaa259e681bc93b80dd4a6ebf331a1170
Michał Łajczak <ml300552@student.polsl.pl>
2023-07-13 23:05:42 +0200

    changes in views

--- lessons/urls.py
+++ lessons/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login-page'),
     path('register/', views.register_page, name='register-page'),
+    path('', views.menu_page, name='menu'),
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
 from .forms import UserForm
+from .models import User
 # Create your views here.
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('menu')
+
+    if request.method == "POST":
+        username = request.POST.get('login')
+        password = request.POST.get('password')
+
+        # try:
+        #     user = User.get.object()
+
     return render(request, 'lessons/login_page.html')
+
 
 def register_page(request):
     form = UserForm()
 
-    return render(request, 'lessons/register_page.html', {'form':form})
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login-page')
+
+    return render(request, 'lessons/register_page.html', {'form': form})
+
+def menu_page(request):
+    return render(request, 'lessons/menu')
