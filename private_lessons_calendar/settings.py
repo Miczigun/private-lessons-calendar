7b694b11bfbb888fa3a0fe30c9fab944086f9d77
Michał Łajczak <ml300552@student.polsl.pl>
2023-09-21 22:18:58 +0200

    created api app and installed django rest framework

--- private_lessons_calendar/settings.py
+++ private_lessons_calendar/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'private_lessons_calendar',
     'lessons.apps.LessonsConfig',
-    'phonenumber_field'
+    'phonenumber_field',
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
